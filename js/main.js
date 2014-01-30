2f01f492074899620fdd9b22ea58a8cbe080d600
An <amouradi@hotmail.com>
2014-01-30 15:35:26 -0500

    view

--- js/main.js
+++ js/main.js
@@ -1,28 +1,10 @@
 var app = {
 
-	renderHomeView : function() {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-
-	findByName: function() {
-	    var self = this;
-	    this.store.findByName($('.search-key').val(), function(employees) {
-	        $('.employee-list').html(self.employeeLiTpl(employees));
-	    });
-	},
-
-
 	initialize : function() {
 		var self = this;
-		self.homeTpl = Handlebars.compile($("#home-tpl").html());
-		self.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-
 		this.store = new MemoryStore(function() {
-			
-			self.renderHomeView();
+			$('body').html(new HomeView(self.store).render().el);
 		});
-		
 	},
 
 	showAlert : function(message, title) {
